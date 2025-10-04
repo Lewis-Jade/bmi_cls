3a103980e48f0ca7b298024de34bd030198713dd
Lewis <mudaidalewis@gmail.com>
2025-10-04 13:51:00 +0300

    more functionality

--- app/src/main/java/com/example/calculator/MainMenu.java
+++ app/src/main/java/com/example/calculator/MainMenu.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +15,8 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         ImageView bmiCalculator = findViewById(R.id.bmicalc);
         ImageView myCalculator = findViewById(R.id.calculator);
-
+        TextView bmiNav = findViewById(R.id.bminav);
+        TextView calcNav = findViewById(R.id.calcnav);
 
 
         bmiCalculator.setOnClickListener(view -> {
@@ -31,6 +33,21 @@ public class MainMenu extends AppCompatActivity {
             overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
 
         });
+
+        bmiNav.setOnClickListener(view -> {
+            Intent intent = new Intent(MainMenu.this,BmiCalculatorActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
+
+        });
+        calcNav.setOnClickListener(view -> {
+            Intent intent = new Intent(MainMenu.this,MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right);
+
+        });
+
+
 
 
     }
