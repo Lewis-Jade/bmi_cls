08ac24a1bc35f0bbbf5c7a982248874bba28da93
Lewis <mudaidalewis@gmail.com>
2025-10-06 13:42:32 +0300

    satisfied with the laout

--- app/src/main/java/com/example/calculator/BmiCalculatorActivity.java
+++ app/src/main/java/com/example/calculator/BmiCalculatorActivity.java
@@ -1,19 +1,30 @@
 package com.example.calculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class BmiCalculatorActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bmi_calculator);
+         ImageView backBtn = findViewById(R.id.back);
+        backBtn.setOnClickListener(view -> finish());
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                finish();
+            }
+        });
 
     }
 }
